27dd62c438f05af987b057df7cf2a03ad792e8f5
dzhv <dz.vysniauskas@gmail.com>
2016-05-20 13:20:24 +0300

    Removing unnecessary definition

--- autoservisas.scala
+++ autoservisas.scala
@@ -13,40 +13,37 @@ class Autoservisas extends Specification {
   case class Automobilis(numeris: String, markė: String, pagaminimoData: DateTime, statusas: String, paskutinėsPeržiūrosData: DateTime) {  }
   case class Mechanikas(vardas: String, pavardė: String, telNr: String, darbai: ListBuffer[Automobilis]) {}
 
-  "With various role syntaxes" >> {
    
-    @context
-    case class AtliktiDarbą(mechanikas: Mechanikas) {
+  @context
+  case class AtliktiDarbą(mechanikas: Mechanikas) {
 
-      // Rolės
-      private val meistras  = mechanikas
-      private val automobilis = mechanikas.darbai.take(1) 
+    // Rolės
+    private val meistras  = mechanikas
+    private val automobilis = mechanikas.darbai.take(1) 
       
 
-      role meistras {
-        def taisytiAutomobilį {
-          automobilis.atsinaujinti
-	  self.darbai = self.darbai.drop(1)
-        }
-      }
-
-      role automobilis {
-        def atsinaujinti {
-          self.paskutinėsPeržiūrosData = DateTime.Now
-          self.statusas = "Tvarkingas"
-        }
+    role meistras {
+      def taisytiAutomobilį {
+        automobilis.atsinaujinti
+	    self.darbai = self.darbai.drop(1)
       }
     }
 
-
-    // Test
-
-    val automibilis = Automobilis("III222", "Mazda 5", DateTime.parse("2013-05-05"), "Sugadintas", DateTime.parse("2013-05-05"))
-    val mechanikas = Mechanikas("Jonas", "Jonaitis", "8699999999", new ListBuffer(automobilis))
-
-    AtliktiDarbą(mechanikas)
-    automobilis.statusas === "Tvarkingas"
-    mechanikas.darbai.length === 0
-		
+    role automobilis {
+      def atsinaujinti {
+        self.paskutinėsPeržiūrosData = DateTime.Now
+        self.statusas = "Tvarkingas"
+      }
+    }
   }
+
+
+  // Test
+
+  val automibilis = Automobilis("III222", "Mazda 5", DateTime.parse("2013-05-05"), "Sugadintas", DateTime.parse("2013-05-05"))
+  val mechanikas = Mechanikas("Jonas", "Jonaitis", "8699999999", new ListBuffer(automobilis))
+
+  AtliktiDarbą(mechanikas)
+  automobilis.statusas === "Tvarkingas"
+  mechanikas.darbai.length === 0
 }
