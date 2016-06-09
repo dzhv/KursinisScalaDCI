12e9e9cf13881b516b2ec17f2eb007b4b04aada7
dzhv <dz.vysniauskas@gmail.com>
2016-06-09 21:09:54 +0300

    Tegu bus ir dviratis

--- transportas.scala
+++ transportas.scala
@@ -52,45 +52,35 @@ class Transportas extends Specification {
     }
 
     autobusas.važiuoti
-  }
+  }   
   
   @context
-  case class VažiuotiTaxi(transportoPriemonė: TransportoPriemonė, vairuotojas: Vairuotojas,
-    keleivis: Keleivis, atstumas: Double) {
-  
-    // Rolės    
-    private val taxiVairuotojas = vairuotojas
-    private val taxiKeleivis = keleivis  
-    private val taxi  = transportoPriemonė
-		
-    role taxi {
+  case class VažiuotiDviračiu(transportoPriemonė: TransportoPriemonė, keleivis: Keleivis, atstumas: Double) {
+    
+	// Rolės        
+    private val dviratininkas = keleivis  
+    private val dviratis  = transportoPriemonė
+
+    role dviratininkas{
+      def pasiimtiDviratį{
+        self.pinigai -= dviratis.kelionėsKaina
+        self.važiuoti
+      }	  
       def važiuoti {
+        dviratis.važiuoti
+      }	  
+    }	
+    role dviratis {
+      def važiuoti {        
         self.kilometražas += atstumas
-        vairuotojas.vairuoti
-        autobusoKeleivis.važiuoti
       }
     }	
-    role taxiVairuotojas{
-      def vairuoti {	
-	self.stažas += atstumas	
-      }
-    }    	
-    role taxiKeleivis {
-      def važiuoti {
-        self.pinigai -= taxi.kelionėsKaina * atstumas
-        taxiVairuotojas.pinigai += taxi.kelionėsKaina * atstumas
-      }	  
-    }
-	
-    taxi.važiuoti
   }
-
+  
   // Test    
-  val taxi = TransportoPriemonė("taxi", 1234, 2)
-  val vairuotojas = Vairuotojas("Jonas", "Jonaitis", 100, 5000)
+  val dviratis = TransportoPriemonė("dviratis", 1234, 2)  
   val keleivis = Keleivis("Petras", "Petraitis", 120, 0)
   
-  VažiuotiTaxi(taxi, vairuotojas, keleivis, 50)
-  vairuotojas.stažas === 150
-  keleivis.pinigai === 120 - 2 * 50
+  VažiuotiDviračiu(dviratis, keleivis, 50) 
+  keleivis.pinigai === 120 - 2
 }
